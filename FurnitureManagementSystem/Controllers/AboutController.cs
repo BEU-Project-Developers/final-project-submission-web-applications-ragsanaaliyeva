72d67c2dea3dd960114e47483df1c1c9e30fb2f4
tabrizl <tabrizl@crocusoft.com>
2025-03-25 14:42:48 +0400

    ilk commit

---
+++ FurnitureManagementSystem/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FurnitureManagementSystem.Controllers
+{
+	public class AboutController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
